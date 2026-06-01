f312adbafd1cb34c8289563e3357703a99e5c976
Stanwang218 <756881257@qq.com>
2026-06-01 10:04:46 +0200

    add script in Alvis

---
+++ project/script/tabstar.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH -A NAISS2025-5-568 -p alvis
+#SBATCH -N 1 --gpus-per-node=A100:1  # We're launching 2 nodes with 8 Nvidia T4 GPUs each
+#SBATCH -o /mimer/NOBACKUP/groups/oovgen/ziyuan/wasp-project/wasp-nlp/project/log/tabstar.out
+#SBATCH -t 0-01:30:00
+
+module purge
+module load Python/3.11.3-GCCcore-12.3.0
+module load virtualenv/20.23.1-GCCcore-12.3.0
+source /mimer/NOBACKUP/groups/oovgen/ziyuan/nlp_project_env/bin/activate
+
+python /mimer/NOBACKUP/groups/oovgen/ziyuan/wasp-project/wasp-nlp/project/tests/tabstar_tests.py
